a4921d2505bc1cb6bbae763c90cdd0cc8657534c
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-30 22:00:10 +0600

    18-9 Advanced logical not operator

---
+++ shortcut.js
@@ -0,0 +1,19 @@
+const isLeader = true;
+
+// if(isLeader === true);
+// if (isLeader){
+// 	console.log('leader ashche!  chair charo');
+// }
+// else{
+// 	console.log('vitore dukte dibi na');
+// }
+
+const isPassed = true;
+//if(isPassed === false)
+
+if (!isPassed){
+	console.log('toke biya diya dimu');
+}
+else{
+	console.log('tumi amader noyon er moni');
+}
