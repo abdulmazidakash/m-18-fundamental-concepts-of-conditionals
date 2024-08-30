7e9f7425119c7df0fa5cbdb44c4bea2a5e00a6c3
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-30 19:59:24 +0600

    18-8 again push previous not saved

--- ternary.js
+++ ternary.js
@@ -31,3 +31,26 @@ else{
 // console.log(price);
 
 price = isLeader === true ? 0 : price + 100;
+
+// optional semi-advanced ternary 
+if (isLeader === true){
+	if (price > 1000) {
+		price = price / 2;
+	}
+	else {
+		price = 0;
+	}
+}
+else {
+	price = price + 1000;
+}
+
+price > 1000 ? price / 2 : 0
+
+//feel free to ignore this one
+
+price = isLeader === true ? price > 1000 ? price / 2 : 0 : price + 1000;
+price = isLeader === true ? 
+			price > 1000 ? 
+				price / 2 : 0 
+       : price + 1000;
