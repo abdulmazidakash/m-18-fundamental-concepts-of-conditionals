7889bdf8ae82846db070794f0617a7a33f2a97b6
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-30 10:23:01 +0600

    18-1 compare variable and comparison operator

---
+++ variable.js
@@ -0,0 +1,29 @@
+//number
+var sunglassPrice = 80;
+
+//string
+var subject =  "EEE";
+
+//boolean
+var passed = true;
+
+/**
+ * Naming Convention
+ * 1. Single word
+ * 2. No quote
+ * 3. No gap or dash
+ * 4. Can not start with number
+ * 5. Can not be a keyword
+ * 6. Prefer camelCase
+ */
+
+var lastExamResult  = 89;
+
+/**
+ * Operation
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN
+ * 4. null
+ * 5. Number
+ */
