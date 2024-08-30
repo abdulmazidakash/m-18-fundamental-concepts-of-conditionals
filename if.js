a6868dc4d4e00f93ff687e71e5890dfafd43294f
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-30 15:17:33 +0600

    18-4 conditional branching if else condition

---
+++ if.js
@@ -0,0 +1,50 @@
+/**
+ * tui jodi amake khawas 
+ * taile ami asbo
+ * 
+ * 
+ * -----------------------------
+ * 
+ * if you buy food for me
+ *  i will come
+ * 
+ * ---------------------
+ * if biriyani price less than 300 taka 
+ *   i will buy
+ * 
+ * --------------------
+ * 
+ * ---------structure-------------
+ * if(condition){
+ * 
+ * }
+ */
+
+// if(3 < 10){
+// 	console.log("i have a smaller value");
+// }
+
+// var biriyaniPrice = 250;
+
+// if(biriyaniPrice < 300){
+// 	console.log("mama give me some biriyani");
+// }
+
+/**
+ * if (condition){
+ * 		execute code if the condition is true
+ * }
+ * else {
+ * 		execute code if the condition is false
+ * 
+ * }
+ */
+
+var weight = 40;
+
+if(weight < 20){
+	console.log('i will carry it by myself');
+}
+else{
+	console.log('i will rent a richshaw');
+}
