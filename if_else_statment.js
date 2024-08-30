e153d48816a629f30b52f87f8338a6a58a193427
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-30 12:34:02 +0600

    18-3 if else statment visualization

---
+++ if_else_statment.js
@@ -0,0 +1,18 @@
+// const age = 10;
+
+// if(age > 10) {
+// 	console.log("your request accept");
+// }
+// else{
+// 	console.log("sorry, your request not accepted");
+// }
+
+var age = 15;
+
+if (age >= 18) {
+	console.log("any ride you go");
+}else if( age >= 13) {
+	console.log("some ride you go");
+} else {
+	console.log("just you go kids ride");
+}
